8c0df01a54dc55e3c593f3dc3034fec457919612
abdulmuzzammilbasha <muzzamil.bsh@gmail.com>
2023-08-18 14:48:50 +0530

    updated

--- day6task/ArrayFunctions.js
+++ day6task/ArrayFunctions.js
@@ -1,7 +1,7 @@
 const currencies = {
     name: "india",
     populatioin: 100,
-    money: usd={name:"american dollarnod"}
+    money: usd: {name:"american dollarnod"}
   };
 const keyArray = Object.keys(currencies);
 console.log(keyArray);
