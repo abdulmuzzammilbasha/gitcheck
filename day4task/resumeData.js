9bc3574d6805c11888e2a4c56326a954a55f5a37
abdulmuzzammilbasha <muzzamil.bsh@gmail.com>
2023-08-18 14:55:15 +0530

    updated

---
+++ day4task/resumeData.js
@@ -0,0 +1,56 @@
+const resumeDate = {
+    "Contact Information": {
+      "Name": "Abdul Muzzammil Basha S",
+      "Email": "muzzamil.bsh@gmail.com",
+      "Phone": "9655905284",
+      "Address": "chennai, Tamil Nadu"
+    },
+    "Education": [
+        {
+            "Degree": "10th Grade",
+            "Major": "Tamil Nadu State board",
+            "School": "Shree Natesan Vidyasala Mat.Hr.School",
+            "GraduationYear": 2014
+        },
+        {
+            "Degree": "12th Grade",
+            "Major": "Biology with Maths",
+            "School": "Shree Natesan Vidyasala Mat.Hr.School",
+            "GraduationYear": 2016
+        },
+      {
+        "Degree": "Bachelor of Science",
+        "Major": "IT & Logistics",
+        "University": "Bharathiyar University",
+        "College": "Indian Institute of Logistics",
+        "GraduationYear": 2019
+      }
+    ],
+    
+    "Work Experience": [
+      {
+        "Title": "Intern",
+        "Company": "Soft Link Global Pvt. Ltd",
+        "Dates": "2019",
+        "Description": "Technical Software support"
+      },
+      {
+        "Title": "Transaction Associate",
+        "Company": "Accenture Solutions",
+        "Dates": "2019-2021",
+        "Description": "Process support to AR and AP of client"
+      }
+    ],
+
+    "Skills": [
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "React",
+      "Problem Solving",
+      "Team player",
+      "Communication"
+    ]
+
+  }
+  
