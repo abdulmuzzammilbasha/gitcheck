e07a8bba256f187843b00c9da0e36c600a912dac
abdulmuzzammilbasha <muzzamil.bsh@gmail.com>
2023-08-19 00:12:58 +0530

    updated

---
+++ day4task/forloops.js
@@ -0,0 +1,42 @@
+const productList = [
+    {
+    "product": "Laptop",
+    "brand": "HP",
+    "price": 43000,
+    "specs": {
+      "processor": "Intel Core i7",
+      "memory": "16GB RAM",
+      "storage": "512GB SSD"
+    }
+},
+{
+    "product": "phone",
+    "brand": "OPPO",
+    "price": 18000,
+    "specs": {
+      "processor": "android",
+      "memory": "8GB RAM",
+      "storage": "64GB memory"
+}
+
+  },
+  {
+    "product": "smart Watch",
+    "brand": "apple",
+    "price": 25000,
+    "specs": {
+      "processor": "i",
+      "memory": "8GB RAM",
+      "storage": "64GB memory"
+}
+
+  }
+]
+
+productList.forEach((item)=>{
+console.log(item.product);
+console.log(item.price);
+console.log(item.specs)
+})
+
+  
